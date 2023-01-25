e39cd096d9b13c48c5bc722cf80d9d669511b971
shohei-kojima <26kojima@gmail.com>
2023-01-25 19:07:00 +0900

    20230125-190700

--- test_plot.py
+++ test_plot.py
@@ -52,5 +52,5 @@ for node in t.traverse():
         nstyle['size']=10
         node.set_style(nstyle)
 
-t.render('test.png', w=100, units='mm', tree_style=ts)
+t.render('test.png', w=200, units='mm', tree_style=ts)
 
